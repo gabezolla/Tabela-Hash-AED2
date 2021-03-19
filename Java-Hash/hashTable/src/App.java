2663f494ae4cbb4aaf080b957b8bc64eb38d95df
gabezolla <gabriel.zolla1998@gmail.com>
2021-03-19 18:50:41 -0300

    Final version developed.

--- Java-Hash/hashTable/src/App.java
+++ Java-Hash/hashTable/src/App.java
@@ -5,41 +5,49 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int m = 200;
-        double A = 0.61803398875;
-        // hashMapDivision h1 = new hashMapDivision(m);
-        hashMapMultiply h2 = new hashMapMultiply(m, A);
-        int collisionCounter = 0;   
+        int m = 97; // Constante m.
+        int keyRange = 10000; // Constante arbitrária que determina o valor máximo para uma chave.
+        double A = 0.61803398875; // Constante A.
+        hashMapDivision h1 = new hashMapDivision(m); // Hash Map implementado para o método da divisão.
+        hashMapMultiply h2 = new hashMapMultiply(m, A); // Hash Map implementado para o método da multiplicação.
+        int collisionCounter = 0; // Contador para colisões.
         Scanner scan = new Scanner(System.in);
 
-        // Generate hash table...       
-        for(int i = 0; i<10000; i++) {  
+        // Gerar a Hash Table...       
+        for(int i = 0; i<100; i++) {  
             
-            // Generate random String...
+            // Gerar uma String aleatória para colocá-la na chave...
             String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+"0123456789"+"abcdefghijklmnopqrstuvxyz";
             StringBuilder sb = new StringBuilder(7);    
 
             for (int j = 0; j < 7; j++) {     
-                int index = (int)(AlphaNumericString.length() * Math.random()); // generate a random number between 0 to AlphaNumericString variable length 
-                sb.append(AlphaNumericString .charAt(index)); // add Character one by one in end of sb 
+                int index = (int)(AlphaNumericString.length() * Math.random()); // Gera um AlphaNumericString. 
+                sb.append(AlphaNumericString .charAt(index)); // Adiciona os caracteres na variável sb.
             } 
             String randomString = sb.toString();
-            // End of String Generator
+            // Fim da String Generator...
 
-            // Generate a random Integer...
-            int randomNumber = Math.round((float)Math.random()*100);
+            // Gerar um número aleatório...
+            int randomNumber = Math.round((float)Math.random()*keyRange);
             Tuple t = new Tuple(randomNumber, randomString);  
+            
+            if(h1.insertOnHash(t) == 1) collisionCounter++;  // Roda o método da divisão e faz a contagem das colisões.
+            // if(h2.insertOnHash(t) == 1) collisionCounter++;  // Roda o método da multiplicação e faz a contagem das colisões.
+            
 
-            // if(h1.insertOnHash(t) == 1) collisionCounter++;  
-            if(h2.insertOnHash(t) == 1) collisionCounter++;  
-                     
+            /* Caso de interação com o usuário...
+            System.out.println("Digite uma chave: ");
+            int typedKey = scan.nextInt();
+            if(h1.insertOnHash(t) == 1) collisionCounter++;  // Roda o método da divisão e faz a contagem das colisões
+            if(h2.insertOnHash(t) == 1) collisionCounter++;  // Roda o método da multiplicação e faz a contagem das colisões            
+            */
         }
 
         System.out.print("\n\n===== NÚMERO DE COLISÕES: ");
         System.out.print(collisionCounter);
         System.out.println(" =====");
 
-
+        // Trecho de código para a busca de chaves... Descomentar caso for usar.
         /* System.out.println("\nDigite uma chave: ");
         int testKey = scan.nextInt();
         while(testKey != -1) {
